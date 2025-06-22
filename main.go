4dc4224791e91ad7387d57cdd908387879677310
Eduardo Barreira <edubarreira1212@gmail.com>
2025-06-21 22:41:05 -0300

    feat: add user registration endpoint to the HTTP server

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crud/server"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,6 +11,8 @@ import (
 
 func main() {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/users", server.CreateUser).Methods(http.MethodPost)
 
 	fmt.Println("Server running on port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
